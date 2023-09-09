4faad41fbeae9a99f1d5cd76c99321ca77cfb49c
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2023-09-09 22:23:37 +0100

    fix logs path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ sed -i 's/api_key: fake-api-key/api_key: '"$DD_API_KEY"'/g' /etc/datadog-agent/d
 sed -i 's/# logs_enabled: false/logs_enabled: true/g' /etc/datadog-agent/datadog.yaml
 sed -i 's/# env: <environment name>/env: prod/g' /etc/datadog-agent/datadog.yaml
 
+mkdir -p /var/log/roodle
+
 service datadog-agent start
 
 /app/bin/app
