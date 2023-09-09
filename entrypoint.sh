07f74686c7672f76dab53219090eed49b485f388
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2023-09-09 22:12:57 +0100

    add logs to datadog

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 sed -i 's/api_key: fake-api-key/api_key: '"$DD_API_KEY"'/g' /etc/datadog-agent/datadog.yaml
+sed -i 's/# logs_enabled: false/logs_enabled: true/g' /etc/datadog-agent/datadog.yaml
+sed -i 's/# env: <environment name>/env: prod/g' /etc/datadog-agent/datadog.yaml
 
 service datadog-agent start
 
